f8d10529ab6002e95759d8af409de2f488c62fc1
zain451 <zain@shefa-solutions.com>
2025-05-27 17:20:18 +0500

    new host name added

--- a_core/settings.py
+++ a_core/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'live-cha.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'live-chat-mg5g.onrender.com']
 
 
-CSRF_TRUSTED_ORIGINS = [ 'https://live-cha.onrender.com' ]
+CSRF_TRUSTED_ORIGINS = [ 'https://live-chat-mg5g.onrender.com' ]
 
 https://live-cha.onrender.com
 # Application definition
