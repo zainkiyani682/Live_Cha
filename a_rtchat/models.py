830bd94ca9470f64eb1536fa38f3900a32991dfa
zain451 <zain@shefa-solutions.com>
2025-05-27 16:20:23 +0500

    socialaccounts add

--- a_rtchat/models.py
+++ a_rtchat/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 class ChatGroup(models.Model):
-    group_name = models.CharField(max_length=128,unique=True,default=shortuuid.uuid)
+    group_name = models.CharField(max_length=128,unique=True)
     groupchat_name = models.CharField(max_length=128, null=True,blank=True)
     admin = models.ForeignKey(User,related_name='groupschat',blank=True, null=True, on_delete=models.SET_NULL)
 
@@ -16,7 +16,10 @@ class ChatGroup(models.Model):
 
     def __str__(self):
         return self.group_name
-    
+    def save(self,*args,**kwargs):
+        if not self.group_name:
+            self.group_name = shortuuid.uuid()
+        super().save(*args,**kwargs)
 class GroupMessage(models.Model):
     group = models.ForeignKey(ChatGroup, related_name='chat_message', on_delete=models.CASCADE)
     author = models.ForeignKey(User,on_delete=models.CASCADE)
