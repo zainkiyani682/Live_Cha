bd5bc68c8acbc51cc6152ab9abddf29fe2a2d352
zain451 <zain@shefa-solutions.com>
2025-05-27 15:40:26 +0500

    server issues resolved host

--- a_core/settings.py
+++ a_core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://live-cha-1.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'live-cha-1.onrender.com']
 
 
 CSRF_TRUSTED_ORIGINS = [ 'https://live-cha-1.onrender.com' ]
